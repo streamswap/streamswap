ef966395fc9d50a9f7358114162dddd47f720638
Matthew Conover <he@mconover.dev>
2021-06-24 14:13:22 -0700

    Playing with material ui react

--- packages/exchange/components/MainLayout.tsx
+++ packages/exchange/components/MainLayout.tsx
@@ -2,10 +2,10 @@ import styles from '../styles/MainLayout.module.css';
 import Head from 'next/head';
 import Footer from '../components/Footer';
 
-export default (args: { children: JSX.Element[], title?: string }) => (
+export default (args: { children: JSX.Element | JSX.Element[]; title?: string }) => (
   <div className={styles.container}>
     <Head>
-      <title>Streamswap{ args.title ? ` - ${args.title}` : ''}</title>
+      <title>Streamswap{args.title ? ` - ${args.title}` : ''}</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <main className={styles.main}>{args.children}</main>
--- packages/exchange/pages/index.tsx
+++ packages/exchange/pages/index.tsx
@@ -1,37 +1,50 @@
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 import MainLayout from '../components/MainLayout';
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import CardContent from '@material-ui/core/CardContent';
 
 /** Home */
 export default () => (
   <MainLayout title="Home">
-    <h1 className={styles.title}>Streamswap</h1>
-
-    <p>
-      Continuous exchange of{' '}
-      <a href="https://superfluid.finance" target="_blank" rel="noopener">
-        Superfliud
-      </a>{' '}
-      streams
-    </p>
-
-    <div className={styles.grid}>
-      <Link href="/exchange">
-        <a className={styles.card}>
-          <h2>Exchange &rarr;</h2>
-          <p>Start exchanging super tokens now!</p>
-        </a>
-      </Link>
-
-      <a className={styles.card}>
-        <h2>Create Exchange &rarr;</h2>
-        <p>Create a new super token exchange.</p>
-      </a>
-
-      <a href="" className={styles.card}>
-        <h2>Documentation &rarr;</h2>
-        <p>Learn about the inner workings of Streamswap.</p>
-      </a>
-    </div>
+    <Grid container spacing={4} >
+      <Grid item xs={12}>
+        <h1 className={styles.title}>Streamswap</h1>
+        <p className={styles.description}>
+          Continuous exchange of{' '}
+          <a href="https://superfluid.finance" target="_blank" rel="noopener">
+            Superfliud
+          </a>{' '}
+          streams
+        </p>
+      </Grid>
+      <Grid item sm={6} xs={12}>
+        <Link href="/exchange">
+          <Card className={styles.card}>
+            <CardContent>
+              <h2>Exchange &rarr;</h2>
+              <p>Start exchanging super tokens now!</p>
+            </CardContent>
+          </Card>
+        </Link>
+      </Grid>
+      <Grid item sm={6} xs={12}>
+        <Card className={styles.card}>
+          <CardContent>
+            <h2>Create Exchange &rarr;</h2>
+            <p>Create a new super token exchange.</p>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item sm={6} xs={12}>
+        <Card className={styles.card}>
+          <CardContent>
+            <h2>Documentation &rarr;</h2>
+            <p>Learn about the inner workings of Streamswap.</p>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   </MainLayout>
 );
