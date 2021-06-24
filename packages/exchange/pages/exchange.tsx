0008dcd0e34928c017af2ff625d22dd58f34450e
Matthew Conover <he@mconover.dev>
2021-06-24 14:55:36 -0700

    Typography and styling

--- packages/exchange/pages/exchange.tsx
+++ packages/exchange/pages/exchange.tsx
@@ -1,6 +1,10 @@
 import styles from '../styles/Exchange.module.css';
 import MainLayout from '../components/MainLayout';
 
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 export default () => (
   <MainLayout title="Exchange">
--- packages/exchange/pages/index.tsx
+++ packages/exchange/pages/index.tsx
@@ -4,44 +4,47 @@ import MainLayout from '../components/MainLayout';
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
 import CardContent from '@material-ui/core/CardContent';
+import T from '@material-ui/core/Typography';
 
 /** Home */
 export default () => (
   <MainLayout title="Home">
-    <Grid container spacing={4} >
+    <Grid container spacing={4}>
       <Grid item xs={12}>
-        <h1 className={styles.title}>Streamswap</h1>
-        <p className={styles.description}>
+        <T className={styles.title} variant="h1">
+          Streamswap
+        </T>
+        <T className={styles.description} variant="subtitle1" component="p">
           Continuous exchange of{' '}
           <a href="https://superfluid.finance" target="_blank" rel="noopener">
             Superfliud
           </a>{' '}
           streams
-        </p>
+        </T>
       </Grid>
-      <Grid item sm={6} xs={12}>
+      <Grid item md={6} xs={12}>
         <Link href="/exchange">
           <Card className={styles.card}>
             <CardContent>
-              <h2>Exchange &rarr;</h2>
-              <p>Start exchanging super tokens now!</p>
+              <T variant="h4">Exchange&nbsp;&rarr;</T>
+              <T variant="body1">Start exchanging super tokens now!</T>
             </CardContent>
           </Card>
         </Link>
       </Grid>
-      <Grid item sm={6} xs={12}>
+      <Grid item md={6} xs={12}>
         <Card className={styles.card}>
           <CardContent>
-            <h2>Create Exchange &rarr;</h2>
-            <p>Create a new super token exchange.</p>
+            <T variant="h4">Create Exchange&nbsp;&rarr;</T>
+            <T variant="body1">Create a new super token exchange.</T>
           </CardContent>
         </Card>
       </Grid>
-      <Grid item sm={6} xs={12}>
+      <Grid item md={6} xs={12}>
         <Card className={styles.card}>
           <CardContent>
-            <h2>Documentation &rarr;</h2>
-            <p>Learn about the inner workings of Streamswap.</p>
+            <T variant="h4">Documentation&nbsp;&rarr;</T>
+            <T variant="body1">Learn about the inner workings of Streamswap.</T>
           </CardContent>
         </Card>
       </Grid>
