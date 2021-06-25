072e05bdcf21f9f398f55a6f56105973fd3f0891
Matthew Conover <he@mconover.dev>
2021-06-25 10:40:15 -0700

    Finish up exchange structure

--- packages/exchange/pages/exchange.tsx
+++ packages/exchange/pages/exchange.tsx
@@ -9,6 +9,8 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
 import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
 
 interface TokenDef {
   readonly symbol: string;
@@ -22,16 +24,16 @@ const placeholderTokens: TokenDef[] = [
 
 const Exchange = () => (
   <MainLayout title="Exchange">
-    <T variant="h2">Exchange</T>
-    <T variant="body1">Continuously exchange one super token for another</T>
+    <Paper className={styles.exchange}>
+      <T variant="h2">Exchange</T>
+      <T variant="body1">Continuously exchange one super token for another</T>
 
-    <form className={styles.exchange}>
       <T variant="h6" className={styles.sectionHeading}>
         From
       </T>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={5}>
-          <TextField required id="fromAmount" label="Amount" fullWidth type="number" />
+        <Grid item xs={12} sm={4}>
+          <TextField required id="from-amount" label="Amount" fullWidth type="number" />
         </Grid>
         <Grid item xs={6} sm={4}>
           <Autocomplete
@@ -39,11 +41,11 @@ const Exchange = () => (
             getOptionLabel={(o: TokenDef) => o.symbol}
             filterOptions={createFilterOptions({ stringify: (o) => `${o.symbol} - ${o.name}` })}
             renderInput={(params) => <TextField {...params} label="Token" />}
-            id="fromToken"
+            id="from-token"
             fullWidth
           />
         </Grid>
-        <Grid item xs={6} sm={3}>
+        <Grid item xs={6} sm={4}>
           <Select id="from-rate-unit" defaultValue="d" fullWidth className={styles.selectInput}>
             <MenuItem value="s">/second</MenuItem>
             <MenuItem value="m">/minute</MenuItem>
@@ -55,13 +57,34 @@ const Exchange = () => (
         </Grid>
       </Grid>
 
-      {/*<T variant="h6" className={styles.sectionHeading}>To</T>*/}
-      {/*<Grid container sapcing={3}>*/}
-      {/*  <Grid item xs={12}>*/}
+      <T variant="h6" className={styles.sectionHeading}>
+        To
+      </T>
+      <Grid container spacing={3}>
+        <Grid item xs={6} sm={4}>
+          <Autocomplete
+            options={placeholderTokens}
+            getOptionLabel={(o: TokenDef) => o.symbol}
+            filterOptions={createFilterOptions({ stringify: (o) => `${o.symbol} - ${o.name}` })}
+            renderInput={(params) => <TextField {...params} label="Token" />}
+            id="to-token"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={6} sm={4}>
+          <TextField id="min-received" label="Min Received" fullWidth type="number" defaultValue="0" />
+        </Grid>
+      </Grid>
 
-      {/*  </Grid>*/}
-      {/*</Grid>*/}
-    </form>
+      <Grid container spacing={3} className={styles.controlRow}>
+        <Grid item xs={6}>
+          <Button className={styles.submitButton}>cancel</Button>
+        </Grid>
+        <Grid item xs={6}>
+          <Button color="primary" variant="contained" className={styles.submitButton}>Exchange</Button>
+        </Grid>
+      </Grid>
+    </Paper>
   </MainLayout>
 );
 
